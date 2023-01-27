089e276e5eef2ed1f1e31b346a325d9ceb7e948f
Van Minh Do <112021890+SirVanMinhDo@users.noreply.github.com>
2023-01-27 11:38:43 -0500

    -s1c2 debuttheme

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Modèle par défaut
+ * 
+ */
+?>
+<?php get_header(); ?>
+<h1>Bien venue au cours de 4W4</h1>
+<p>Je suis Van Minh Do. Je suis pas très fort en WEB, mais je mets de l'EFFORT !</p>
+<?php get_footer(); ?>
