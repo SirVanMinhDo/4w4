ea4fdeb0c14bbcc2c2b7cdaf4fae58be8e7f6a0d
Van Minh Do <112021890+SirVanMinhDo@users.noreply.github.com>
2023-02-17 09:41:16 -0500

    S4C2 Ajout des catégories Cours et Note de cours puis leur affichage par is_category()

--- category.php
+++ category.php
@@ -7,21 +7,19 @@
 ?>
 <?php get_header(); ?>
 <main>
-    <h3>front-page.php</h3>
+    <h3>category.php</h3>
     <section class="blockflex">
         <?php
         if (have_posts()) :
             while (have_posts()) : the_post(); ?>
                 <article>
-
-                    <h3>
+                    <h2>
                         <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
-                    </h3>
+                    </h2>
                     <?php // the_content();  // affiche le contenu complet de l'article 
                     ?>
                     <?php // the_excerpt();   // affiche un résumé de l'article 
                     ?>
-                    <p><?php if (is_category('cours')) echo 'cours'; ?> </p>
                     <p><?= wp_trim_words(get_the_excerpt(), 5, "&#9760;") ?></p>
                     <hr>
                 </article>
