084dbc1985536c9e2c569240d32445dc0c329240
Van Minh Do <112021890+SirVanMinhDo@users.noreply.github.com>
2023-03-17 10:02:32 -0400

    s8c2 changement couleur menu aside, burger et ajustement du padding. Changement couleurs et tailles polices du footer.

--- footer.php
+++ footer.php
@@ -1,48 +1,45 @@
 <footer class="site__footer">
     <section>
         À propos
-        <div class="projet_conteneur1">
-            <p>Je me nomme Van Minh Do, un étudiant du Collège de Maisonneuve.
-                Mon rêve est raconter une histoire. Une histoire parlant du
-                passé, du présent et du future par la 3D. Je veux que mon récit
-                soit plein d'émotions grâce à son esthétisme et à sa profondeur.
+        <div class="footer_section1">
+            <p>- Je suis Van Minh Do <br> - J'adore les jeux vidéos <br>- Je veux
+                raconter mon histoire
             </p>
 
         </div>
     </section>
     <section>
         Mon parcours artistique
-        <p>Au primaire, je dessinais, dans mon temps libre, des scènes d'action,
-            comme des invasions de chateaux et des combats entre guerriers. Ma
-            flame pour le dessin s'éteignit à mon arrivé au secondaire.
-            Heureusement, grâce au TIM, j'ai redécouvert cette passion. Désormais,
-            malgré mon manque d'adresse pour le dessin, j'ai décidé de rendre
-            mes gribouillages d'enfant par le biais de la 3D. Présentement,
-            j'étudie l'animation 3D en TIM. J'èspère un jour trouver un emploi
-            dans l'industrie.
-        </p>
+        <div class="footer_section2">
+            <p>- Dessin au primaire <br> - Cours Art plastique au secondaire <br>
+                - cours TIM d'Imagerie et Animation 3D
+            </p>
+        </div>
     </section>
     <section>
         Mes projets
-        <p>Vous trouverez touts mes projets dans les liens suivants:
-        </p>
-        <p>Projets réalisés dans le cours d'Imagerie 3D
-            <a href="https://youtu.be/lPqpCrwRFe4">Tp1 2022</a>
-            <a href="https://youtu.be/oHLoaJj2vsU">Tp2 2022</a>
-        </p>
-        <p>Projets réalisés dans le cours d'Animation 3D
-            <a href="https://youtu.be/hRVUKHM30Us">Tp1 2023</a>
-            <a href="url">Lien Youtube à venir</a>
-        </p>
-
+        <div class="footer_section3">
+            <p>- Projets réalisés dans le cours d'Imagerie 3D
+                <br>
+                <a href="https://youtu.be/lPqpCrwRFe4">Tp1 2022</a>
+                <a href="https://youtu.be/oHLoaJj2vsU">Tp2 2022</a>
+            </p>
+            <p>- Projets réalisés dans le cours d'Animation 3D
+                <br>
+                <a href="https://youtu.be/hRVUKHM30Us">Tp1 2023</a>
+                <a href="url">Lien Youtube à venir</a>
+            </p>
+        </div>
     </section>
     <section>
         Me contacter
-        <p>Portfolios en tant qu'artiste 3D:
-            <a href="https://www.artstation.com/sirvanminhdo">ArtStation</a>
-            <a href="https://sirvanminh.itch.io/">Itch.io</a>
-        </p>
-
+        <div class="footer_section4">
+            <p>- Portfolios en tant qu'artiste 3D:
+                <a href="https://www.artstation.com/sirvanminhdo">ArtStation</a>
+                <br>
+                <a href="https://sirvanminh.itch.io/">Itch.io</a>
+            </p>
+        </div>
     </section>
 </footer>
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -30,7 +30,9 @@
                 )) ?>
 
                 <?php get_search_form(); ?>
-                <label class="burger" for="chkMenu"><img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="20" height="20"></label>
+                <label class="burger" for="chkMenu"><svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="orange">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg></label>
 
             </div>
         </section>
