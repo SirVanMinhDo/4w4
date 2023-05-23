95941362d7ea7bebc02b4dda0819f77d7f976ad6
Van Minh Do <112021890+SirVanMinhDo@users.noreply.github.com>
2023-05-23 13:05:43 -0400

    Mise en page des pages ateliers et ajustement du contenu des textes. Animation hover du menu atelier front-page sur mobile

--- template_atelier.php
+++ template_atelier.php
@@ -8,22 +8,19 @@
 <main class="site__main">
     <?php
     if (have_posts()) : the_post(); ?>
-
         <h1><?= get_the_title(); ?></h1>
-
-        <?php the_post_thumbnail('large'); ?>
-
+        <?php the_post_thumbnail('medium'); ?>
         <?php the_content(); ?>
 
-        <p>Le formateur de l'atelier est <?php the_field('formateur'); ?></p>
+        <p>Le formateur de l'atelier est <?php the_field('formateur'); ?>.</p>
 
-        <p>La date de l'atelier est le <?php the_field('date_debut'); ?></p>
+        <p>La date de l'atelier est le <?php the_field('date_debut'); ?>.</p>
 
-        <p>L'heure à laquelle l'atelier débute est à <?php the_field('heure_de_debut') ?></p>
+        <p>L'heure à laquelle l'atelier débute est à <?php the_field('heure_de_debut') ?>.</p>
 
-        <p>L'atelier dure <?php the_field('duree_dune_seance') ?></p>
+        <p>L'atelier dure <?php the_field('duree_dune_seance') ?> minutes.</p>
 
-        <p>L'atelier aura lieu dans le local <?php the_field('local') ?></p>
+        <p>L'atelier aura lieu dans le local <?php the_field('local') ?>.</p>
 
     <?php endif; ?>
 </main><!-- #main -->
